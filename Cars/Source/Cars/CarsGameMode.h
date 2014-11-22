f5361b8b0e98682e751d9c4d5eed58c213fdf275
Anthony Salani <asalani93@gmail.com>
2014-11-22 15:18:40 -0500

    creates unreal project

---
+++ Cars/Source/Cars/CarsGameMode.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "CarsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARS_API ACarsGameMode : public AGameMode
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
