4ea4287c10549f3a25b9ce0d27d1d039d01c72be
Misael Moscat <misael_moscat@student.uml.edu>
2014-12-01 08:22:47 -0500

    Forgot to comment these two

--- Simulation/include/AStarMap.h
+++ Simulation/include/AStarMap.h
@@ -4,12 +4,14 @@
 #include <vector>
 #include <map>
 
+// Node to use in explored and nonexplored list and priority_queue of A*
 struct Node {
 	struct Node *parent;
 	WorldPos pos;
 	double f, g, h;
 };
 
+// This class is used to compare 2 nodes in a priority_queue, CompareNode(n1, n2) is basically n1.f < n2.f
 class CompareNode {
 public:
 	bool operator()(Node  *n1, Node *n2)
--- Simulation/src/AStarMap.cpp
+++ Simulation/src/AStarMap.cpp
@@ -14,6 +14,7 @@ void AStarMap::makePath(WorldPos startPos, WorldPos endPos, std::vector<WorldPos
      std::priority_queue<Node *, std::vector<Node *>, CompareNode> unexplored;
      double totalDist = getDistance(startPos, endPos);
 
+     // Create start node to add to the unexplored set
      Node *start = new Node();
      start->parent = 0;
      start->pos = startPos;
@@ -21,21 +22,29 @@ void AStarMap::makePath(WorldPos startPos, WorldPos endPos, std::vector<WorldPos
      start->h = totalDist;
      start->f = 0;
 
+     // Add start node to unexplored set
      unexplored.push(start);
-     
+
+     // Loop until unexplored is empty
      while (!unexplored.empty()) {
+	  // Get the top of the unexplored set. Note: unexplored set is a priority queue
+	  // It uses CompareNode(n1, n2) = n1.f < n2.f to set up the queue, so top should be the node
+	  // with the lowest 'f' value
 	  Node *top = unexplored.top();
 	  int parent = getNodeIndex(top->pos);
 	  std::vector<WorldPos> succ = m_adjacents[parent];
 
 	  unexplored.pop();
-	  
+
+	  // Loop through the node's successors, excluding the node itself
 	  for (std::vector<WorldPos>::iterator it = succ.begin(); it != succ.end(); it++) {
 	       if (getDistance(*it, top->pos) == 0) {
 		    continue;
 	       }
 	       
 	       bool ignore = false;
+
+	       // Calculate the h and g values and set up a child node for the sets
 	       double h = getDistance(*it, endPos);
 	       double g = top->g + getDistance(*it, top->pos);
 		    
@@ -45,12 +54,14 @@ void AStarMap::makePath(WorldPos startPos, WorldPos endPos, std::vector<WorldPos
 	       child->g = g;
 	       child->h = h;
 	       child->f = g + h;
-	       
+
+	       // If h (distance to endPos (goal)) is 0, then this is the goal state, so we return
 	       if (h == 0) {
 		    generatePath(child, outList);
 		    return;
 	       }
 
+	       // If a node exists with the same position and a lower 'f' value then don't add this one to the unexplored set
 	       for (std::vector<Node *>::iterator check = explored.begin(); check != explored.end(); check++) {
 		    if (getDistance((*check)->pos, child->pos) == 0) {
 			 if ((*check)->f < child->f) {
@@ -59,7 +70,8 @@ void AStarMap::makePath(WorldPos startPos, WorldPos endPos, std::vector<WorldPos
 			 }
 		    }
 	       }
-	       
+
+	       // If we are not adding this one to the unexplored set, then delete it
 	       if (ignore) {
 		    delete child;
 		    continue;
@@ -68,6 +80,7 @@ void AStarMap::makePath(WorldPos startPos, WorldPos endPos, std::vector<WorldPos
 	       unexplored.push(child);
 	  }
 
+	  // We have now explored the node we poped from the priority_queue
 	  explored.push_back(top);
      }
 }
@@ -78,10 +91,13 @@ double AStarMap::getDistanceFromPath(WorldPos pos) {
 }
 
 double AStarMap::getDistance(WorldPos from, WorldPos to) {
+     // Return the distance between two WorldPos
      return sqrt(pow(from.x - to.x, 2) + pow(from.y - to.y, 2));
 }
 
 int AStarMap::getNodeIndex(WorldPos node) {
+     // This function returns the index of a WorldPos in the m_nodes member of this class
+     // I assume the int key in the m_adjacents is this index
      int index = 0;
      
      for (std::vector<WorldPos>::iterator it = m_nodes.begin(); it != m_nodes.end(); it++, index++) {
@@ -94,6 +110,8 @@ int AStarMap::getNodeIndex(WorldPos node) {
 }
 
 void AStarMap::generatePath(Node *node, std::vector<WorldPos> &outList) {
+     // Tnis function converts the Node linked list into a vector, and reverses it
+     // so that the startPos is at the first item and endPos is the last item.
      while (node != 0) {
 	  outList.push_back(node->pos);
 	  node = node->parent;
